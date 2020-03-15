c5660f5152c660c2e3f2a843a13868fbd96df173
Anderson <andersonst.dev@gmail.com>
2020-03-15 15:29:24 -0300

    Authentication loading

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,32 @@
+import { takeLatest, all, call, put } from 'redux-saga/effects';
+
+import { signInSuccess, signFailure } from './actions';
+
+import api from '~/services/api';
+import history from '~/services/history';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (!user.provider) {
+      console.tron.error('Usuário não é prestador');
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
